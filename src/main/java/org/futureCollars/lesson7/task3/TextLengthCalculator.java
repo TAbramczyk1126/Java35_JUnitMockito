4b9689789c825df9478e4843fee0798aee541382
T.Abramczyk1126 <t.abramczyk@gmail.com>
2024-02-13 23:09:42 +0100

    Task 3(4) - TextLengthCalculator

--- src/main/java/org/futureCollars/lesson7/task3/TextLengthCalculator.java
+++ src/main/java/org/futureCollars/lesson7/task3/TextLengthCalculator.java
@@ -8,6 +8,5 @@ public class TextLengthCalculator {
 
     public static int calculateTextLength(String text) {
         return text.length();
-
     }
 }
--- src/test/java/org/futureCollars/lesson7/task3/TextLengthCalculatorTest.java
+++ src/test/java/org/futureCollars/lesson7/task3/TextLengthCalculatorTest.java
@@ -1,30 +1,30 @@
 package org.futureCollars.lesson7.task3;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.Arguments;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class TextLengthCalculatorTest {
 
-   private static Stream<Arguments> provideStringsForGetTextLength(){
-       return Stream.of(
-               Arguments.of("Test", 4),
-               Arguments.of("", ),
-               Arguments.of("", ),
-               Arguments.of("", )
-       )
-   }
+    private static Stream<Arguments> provideStringsForGetTextLength() {
+        return Stream.of(
+                Arguments.of("Test", 4),
+                Arguments.of("Future Collar", 13),
+                Arguments.of("Mockito", 7),
+                Arguments.of("", 0)
+        );
+    }
 
     @ParameterizedTest
-   @MethodSource ("provideStringsFormGetTextLength")
-   void shouldReturnTextLength(String input, int expected){
-       int actualLength = TextLengthCalculator.calculateTextLength(input);
+    @MethodSource("provideStringsForGetTextLength")
+    void shouldReturnTextLength(String input, int expected) {
+        //given
+        //when
+        int actualLength = TextLengthCalculator.calculateTextLength(input);
+        //then
         Assertions.assertEquals(expected, actualLength);
     }
 }
