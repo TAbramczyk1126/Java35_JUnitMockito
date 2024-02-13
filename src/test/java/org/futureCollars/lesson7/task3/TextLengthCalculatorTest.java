e31893d3229b38dc580e46cb787830fd4a72f306
T.Abramczyk1126 <t.abramczyk@gmail.com>
2024-02-13 08:18:06 +0100

    Final version - task 2

---
+++ src/test/java/org/futureCollars/lesson7/task3/TextLengthCalculatorTest.java
@@ -0,0 +1,18 @@
+package org.futureCollars.lesson7.task3;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TextLengthCalculatorTest {
+
+   /* @ParameterizedTest
+    @MethodSource ("Tomek")
+   void shouldReturnTextLength(String input, int expected){
+        String result = input.trim().text.length;
+        Assertions.assertEquals(expected,result);
+    }*/
+}
