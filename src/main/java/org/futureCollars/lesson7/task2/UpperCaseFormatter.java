2e34bf97e49cafbd5051559c7714a791f2592c52
T.Abramczyk1126 <t.abramczyk@gmail.com>
2024-02-13 08:14:55 +0100

    Final version - task 2

---
+++ src/main/java/org/futureCollars/lesson7/task2/UpperCaseFormatter.java
@@ -0,0 +1,10 @@
+package org.futureCollars.lesson7.task2;
+
+public class UpperCaseFormatter {
+    public static String toUpperCase(String text) {
+        if (text == null) {
+            return "";
+        }
+        return text.toUpperCase();
+    }
+}
